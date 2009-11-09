c78f0f2580c709249b2a49b0b871ab3ceafcab04
heru.ekos <heru.ekos@8717e0ce-c0f7-11de-acfc-b766374499fb>
2009-11-09 06:11:18 +0000

    tambahan reporting/pdf

---
+++ reports/pdf/lap2.php
@@ -0,0 +1,47 @@
+<?php
+include "../../fpdf.php";
+
+class PDF extends FPDF
+{
+    function Header(){
+        $this->SetFont("Calligrapher", "", 20);     
+        $this->Cell(0, 10, "Hello World of Report", 0, 0, 'C');
+        $this->Ln(10);
+    }
+    
+    function __construct(){
+        $this->FPDF('l');
+        $this->AddFont("Calligrapher", '', 'calligra.php');
+    }
+    
+    function add_table(){
+        $this->SetFont("Times",'',10);
+        
+        $this->Cell(0, 5, "Nama : Joni", 0, 0,'L');
+        $this->Cell(80);
+        $this->Cell(0, 5, "Alamat : Jl. Nangka 10 \nSurabaya", 0, 0,'R');
+        $this->Ln();
+        $this->Cell(0, 5, "Kec : Ploso", 0, 0,'L');
+        $this->Ln();
+        $this->Cell(0, 5, "Kec : Ploso", 0, 0,'L');
+        $this->Ln();
+        $this->Cell(0, 5, "Kec : Ploso", 0, 0,'L');
+        $this->Ln();
+        $this->Cell(0, 5, "Kec : Ploso", 0, 0,'L');
+        $this->Ln(10);
+        $w = array(55, 55, 55, 55, 55);
+        $this->SetLineWidth(0.3);
+        $header=array('Country','Capital','Area (sq km)','Pop. (thousands)', 'django');
+        
+        for($i = 0; $i < count($w); $i++){
+            $this->Cell($w[$i], 10, $header[$i], 1, 0,'C');
+        }
+        $this->Ln();
+    }
+}
+
+$pdf = new PDF();
+$pdf->AddPage();
+$pdf->add_table();
+$pdf->Output();
+?>
