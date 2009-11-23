8f22ecbf06a8a3bf24c7da4d7e8cebe4389cd99b
heru.ekos <heru.ekos@8717e0ce-c0f7-11de-acfc-b766374499fb>
2009-11-23 23:46:20 +0000

    modify reporting

--- trunk/reports/report_generator.php
+++ trunk/reports/report_generator.php
@@ -1,5 +1,5 @@
 <?php
-
+include_once "../includes/helpers.inc.php";
 $request = $_GET['q'];
 switch($request)
 {
@@ -58,16 +58,33 @@ switch($request)
         echo $chart->toPrettyString();
         break;
     case "3": // pekerjaaan
-        srand((double)microtime()*1000000);
-        $data = array();
         
-        // add random height bars:
-        for( $i=0; $i<10; $i++ )
-          $data[] = rand(2,9);
+        $sql = "select count(k.pekerjaan) as count, k.pekerjaan as pekerjaan from  pekerjaan k,
+        penduduk p, alamat a, keluarga l, kelurahan kel where p.pekerjaan_id = k.id
+        and p.keluarga_id = l.id and l.alamat_id = a.id and a.kelurahan_id = kel.id
+        and kel.kecamatan_id = 4 group by k.id";
+        $conn = MysqlManager::get_connection();
+        $result = $conn->query($sql);
+        check_error($conn);
+        $ds = array();
+        $length = 0;
+        while($row = $result->fetch_object()){
+            $ds[] = array($row->count, $row->pekerjaan);
+            $length++;
+        }
+        MysqlManager::close_connection($conn);
+        
+        $data = array();
+       
+        for($i=0; $i <  $length; $i++ )
+        {
+            $arr = $ds[$i];
+            $data[] = (int)$arr[0];
+        }
         
         require_once('../php-ofc-library/open-flash-chart.php');
         
-        $title = new title("Pekerjaan");
+        $title = new title("Pekerjaan".$_GET['kecamatan_id']);
         
         $bar = new bar();
         $bar->set_values( $data );
@@ -75,7 +92,12 @@ switch($request)
        
         $x_axis = new x_axis();
         $x_axis->set_colour('#909090');
-        $x_axis->set_labels(array(1,2,3,4,5,6,7,8,9,10));
+        $labels = array();
+        for($i = 0; $i < $length; $i++){
+            $arr = $ds[$i];
+            $labels[] = $arr[1];
+        }
+        $x_axis->set_labels_from_array($labels);
         
         $chart = new open_flash_chart();
         $chart->set_title( $title );
@@ -83,6 +105,7 @@ switch($request)
         $chart->set_x_axis($x_axis );
         
         echo $chart->toPrettyString();
+        
         break;
     case "4": // penghasilan
         srand((double)microtime()*1000000);
@@ -94,7 +117,7 @@ switch($request)
         
         require_once('../php-ofc-library/open-flash-chart.php');
         
-        $title = new title("Penghasilan Penduduk");
+        $title = new title("Penghasilan Penduduk ");
         
         $bar = new bar();
         $bar->set_values( $data );
@@ -117,18 +140,22 @@ switch($request)
         echo $chart->toPrettyString();
         break;
     case "5":
-        include "../includes/helpers.inc.php";
+        
+        $additional = array();
+        $additional[] = array("key"=>'all', "value"=>"All");
         echo "<li><label for='kecamatan'>Kecamatan</label>";
-        echo select("kecamatan", "id", "nama_kecamatan","kecamatan",'class="ui-widget-content ui-corner-all"');
+        echo select("kecamatan", "id", "nama_kecamatan","kecamatan",'class="ui-widget-content ui-corner-all"',"",-1,$additional);
         echo "</li><li><label for='kelurahan' id='lbl_desa'>Desa/Kelurahan</label></li>";		       
         break;
     case "6":
-        include "../includes/helpers.inc.php";
+        
         $kec_id = $_GET['kecamatan_id'];
-        echo select("kelurahan", "id", "nama_kelurahan", "kelurahan",
-            'class="ui-widget-content ui-corner-all"',
-            "kecamatan_id = $kec_id");
+        if($kec_id != "all"){
+            $additional = array();
+            $additional[] = array("key"=>'all', "value"=>"All");
+            echo select("kelurahan", "id", "nama_kelurahan", "kelurahan",
+                'class="ui-widget-content ui-corner-all"',
+                "kecamatan_id = $kec_id",-1, $additional);
+        }        
         break;
 }
-
-
